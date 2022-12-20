88820ad6a56fe6e2a08c20b64ad1722b1c10e2d0
bash62 <bash62@protonmail.com>
2022-12-20 15:05:46 +0100

    End of HTTP-server

--- internal/main.go
+++ internal/main.go
@@ -2,73 +2,72 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 
+	"github.com/bash62/learn-go-http/pkg/swagger/server/restapi"
+	"github.com/bash62/learn-go-http/pkg/swagger/server/restapi/operations"
 	"github.com/go-openapi/loads"
-    "github.com/go-openapi/runtime/middleware"
-    "github.com:bash62/learn-go-http/pkg/swagger/server/restapi"
-    "github.com:bash62/learn-go-http/pkg/swagger/server/restapi/operations"
+	"github.com/go-openapi/runtime/middleware"
 )
 
 func main() {
 
-    // Initialize Swagger
-    swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
-    if err != nil {
-        log.Fatalln(err)
-    }
+	// Initialize Swagger
+	swaggerSpec, err := loads.Analyzed(restapi.SwaggerJSON, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-    api := operations.NewHelloAPIAPI(swaggerSpec)
-    server := restapi.NewServer(api)
+	api := operations.NewHelloAPIAPI(swaggerSpec)
+	server := restapi.NewServer(api)
 
-    defer func() {
-        if err := server.Shutdown(); err != nil {
-            // error handle
-            log.Fatalln(err)
-        }
-    }()
+	defer func() {
+		if err := server.Shutdown(); err != nil {
+			// error handle
+			log.Fatalln(err)
+		}
+	}()
 
-    server.Port = 8080
+	server.Port = 8080
 
-    api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
+	api.CheckHealthHandler = operations.CheckHealthHandlerFunc(Health)
 
-    api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
+	api.GetHelloUserHandler = operations.GetHelloUserHandlerFunc(GetHelloUser)
 
-    api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
+	api.GetGopherNameHandler = operations.GetGopherNameHandlerFunc(GetGopherByName)
 
-    // Start server which listening
-    if err := server.Serve(); err != nil {
-        log.Fatalln(err)
-    }
+	// Start server which listening
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
+	}
 }
 
-//Health route returns OK
+// Health route returns OK
 func Health(operations.CheckHealthParams) middleware.Responder {
-    return operations.NewCheckHealthOK().WithPayload("OK")
+	return operations.NewCheckHealthOK().WithPayload("OK")
 }
 
-//GetHelloUser returns Hello + your name
+// GetHelloUser returns Hello + your name
 func GetHelloUser(user operations.GetHelloUserParams) middleware.Responder {
-    return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
+	return operations.NewGetHelloUserOK().WithPayload("Hello " + user.User + "!")
 }
 
-//GetGopherByName returns a gopher in png
+// GetGopherByName returns a gopher in png
 func GetGopherByName(gopher operations.GetGopherNameParams) middleware.Responder {
 
-    var URL string
-    if gopher.Name != "" {
-        URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
-    } else {
-        //by default we return dr who gopher
-        URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
-    }
+	var URL string
+	if gopher.Name != "" {
+		URL = "https://github.com/scraly/gophers/raw/main/" + gopher.Name + ".png"
+	} else {
+		//by default we return dr who gopher
+		URL = "https://github.com/scraly/gophers/raw/main/dr-who.png"
+	}
 
-    response, err := http.Get(URL)
-    if err != nil {
-        fmt.Println("error")
-    }
+	response, err := http.Get(URL)
+	if err != nil {
+		fmt.Println("error")
+	}
 
-    return operations.NewGetGopherNameOK().WithPayload(response.Body)
+	return operations.NewGetGopherNameOK().WithPayload(response.Body)
 }
